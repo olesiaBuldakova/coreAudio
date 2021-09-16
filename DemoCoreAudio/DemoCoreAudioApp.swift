350d988e90460a1910d36db4de4e5a96a5dfdd58
olesya buldakova <lesyabuld@gmail.com>
2021-09-16 19:26:13 +0300

    Initial Commit

---
+++ DemoCoreAudio/DemoCoreAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCoreAudioApp.swift
+//  DemoCoreAudio
+//
+//  Created by Леся Булдакова on 16.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoCoreAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
